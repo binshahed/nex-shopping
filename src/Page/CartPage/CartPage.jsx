ab4ac96dcf0f9e3366e1a20f3fb5470c37ce270e
Shahed <mdbinshahed5@gmail.com>
2024-02-07 01:39:31 +0600

    product detail

---
+++ src/Page/CartPage/CartPage.jsx
@@ -0,0 +1,9 @@
+const CartPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-center font-bold py-10">Your cart</h1>
+    </div>
+  );
+};
+
+export default CartPage;
--- src/features/productDetail/ProductDetail.jsx
+++ src/features/productDetail/ProductDetail.jsx
@@ -1,5 +1,8 @@
+import { Link } from "react-router-dom";
 import Rating from "../../components/productCard/Rating";
 import { discountCalculator } from "../../utils/discountCalculator";
+import Button from "../../components/buttons/Button";
+import ProductDetailCart from "./ProductDetailCart";
 
 const ProductDetail = () => {
   const price = 99;
@@ -9,7 +12,7 @@ const ProductDetail = () => {
     <div>
       {/* basic detail  */}
       <div>
-        <p className="text-3xl font-black">
+        <p className="text-2xl font-black">
           High-performance smartphone with advanced features
         </p>
         <Rating ratings="4" />
@@ -22,7 +25,7 @@ const ProductDetail = () => {
             ${price}
           </span>
           <span className=" text-lg ml-5 bg-red bg-opacity-10 px-5 py-1 rounded-xl text-red ">
-            {discountPercent}%
+            -{discountPercent}%
           </span>
         </p>
         <p className="my-5">
@@ -30,11 +33,41 @@ const ProductDetail = () => {
           soft and breathable fabric, it offers superior comfort and style.
         </p>
       </div>
-      <hr className="h-px my-8 bg-placeholder border-0" />
+      <hr className="h-px my-4 bg-gray1 border-0" />
       {/* color  */}
       <div>
-            <p className="text-lg ">Select Colors</p>
+        <p className="text-lg text-placeholder ">Select Colors</p>
+        <ul className="flex">
+          <Link className="mr-2" to="/">
+            <li className="p-4 bg-red rounded-full  border-primary border-2"></li>
+          </Link>
+          <Link to="/" className="mr-2">
+            <li className="p-4 bg-primary rounded-full  border-primary border-2"></li>
+          </Link>
+          <Link to="/" className="mr-2">
+            <li className="p-4 bg-white rounded-full  border-primary border-2"></li>
+          </Link>
+        </ul>
       </div>
+      <hr className="h-px my-4 bg-gray1 border-0" />
+      {/* Size  */}
+      <div>
+        <p className="text-lg text-placeholder ">Size</p>
+        <Button bg="gray1" styles="mr-2 mb-2 md:mb-0">
+          Small
+        </Button>
+        <Button bg="gray1" styles="mr-2 mb-2 md:mb-0">
+          Medium
+        </Button>
+        <Button bg="gray1" styles="mr-2 mb-2 md:mb-0">
+          Large
+        </Button>
+        <Button bg="gray1" styles="mr-2 mb-2 md:mb-0">
+          X-Large
+        </Button>
+      </div>
+      <hr className="h-px my-4 bg-gray1 border-0" />
+      <ProductDetailCart />
     </div>
   );
 };
