e5ae68ba8ba4d82ff01fc99ec3fa449713dbf512
binshahed <mdbinshahed5@gmail.com>
2024-03-17 22:06:15 +0600

    dark mood false

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 import colors from "tailwindcss/colors";
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false,
   theme: {
     container: {
       center: true,
@@ -10,15 +11,15 @@ export default {
         sm: "2rem",
         lg: "4rem",
         xl: "5rem",
-        "2xl": "6rem",
-      },
+        "2xl": "6rem"
+      }
     },
     extend: {},
     colors: {
       transparent: "transparent",
       primary: colors.black,
-      ...colors,
-    },
+      ...colors
+    }
   },
-  plugins: [],
+  plugins: []
 };
